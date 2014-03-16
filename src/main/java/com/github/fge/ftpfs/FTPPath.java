4d196dbbfe35bdb74b8e1003cc076d3262a6b20e
Francis Galiegue <fgaliegue@gmail.com>
2014-03-16 17:08:23 +0100

    FTPPath: make abstract
    Given the number of methods which are dependent on whether the path is only a
    filename, a directory plus a filename, a "dot dot" path, an absolute path etc,
    making one and only one class is waaay too complicated.

--- src/main/java/com/github/fge/ftpfs/FTPPath.java
+++ src/main/java/com/github/fge/ftpfs/FTPPath.java
@@ -20,174 +20,60 @@ package com.github.fge.ftpfs;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.FileSystem;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.Iterator;
 
-public final class FTPPath
+public abstract class FTPPath
     implements Path
 {
-    @Override
-    public FileSystem getFileSystem()
+    private final FTPFileSystem fs;
+
+    protected FTPPath(final FTPFileSystem fs)
     {
-        return null;
+        this.fs = fs;
     }
 
     @Override
-    public boolean isAbsolute()
+    public final FileSystem getFileSystem()
     {
-        return false;
+        return fs;
     }
 
     @Override
-    public Path getRoot()
+    public final File toFile()
     {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
-    public Path getFileName()
-    {
-        return null;
-    }
-
-    @Override
-    public Path getParent()
-    {
-        return null;
-    }
-
-    @Override
-    public int getNameCount()
-    {
-        return 0;
-    }
-
-    @Override
-    public Path getName(int index)
-    {
-        return null;
-    }
-
-    @Override
-    public Path subpath(int beginIndex, int endIndex)
-    {
-        return null;
-    }
-
-    @Override
-    public boolean startsWith(Path other)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean startsWith(String other)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean endsWith(Path other)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean endsWith(String other)
-    {
-        return false;
-    }
-
-    @Override
-    public Path normalize()
-    {
-        return null;
-    }
-
-    @Override
-    public Path resolve(Path other)
-    {
-        return null;
-    }
-
-    @Override
-    public Path resolve(String other)
-    {
-        return null;
-    }
-
-    @Override
-    public Path resolveSibling(Path other)
-    {
-        return null;
-    }
-
-    @Override
-    public Path resolveSibling(String other)
-    {
-        return null;
-    }
-
-    @Override
-    public Path relativize(Path other)
-    {
-        return null;
-    }
-
-    @Override
-    public URI toUri()
-    {
-        return null;
-    }
-
-    @Override
-    public Path toAbsolutePath()
-    {
-        return null;
-    }
-
-    @Override
-    public Path toRealPath(LinkOption... options)
+    public final WatchKey register(final WatchService watcher,
+        final WatchEvent.Kind<?>[] events,
+        final WatchEvent.Modifier... modifiers)
         throws IOException
     {
-        return null;
+        throw new IOException("operation not supported");
     }
 
     @Override
-    public File toFile()
-    {
-        return null;
-    }
-
-    @Override
-    public WatchKey register(WatchService watcher, WatchEvent.Kind<?>[] events,
-        WatchEvent.Modifier... modifiers)
+    public final WatchKey register(final WatchService watcher,
+        final WatchEvent.Kind<?>... events)
         throws IOException
     {
-        return null;
+        throw new IOException("operation not supported");
     }
 
     @Override
-    public WatchKey register(WatchService watcher, WatchEvent.Kind<?>... events)
-        throws IOException
+    public final Iterator<Path> iterator()
     {
         return null;
     }
 
     @Override
-    public Iterator<Path> iterator()
-    {
-        return null;
-    }
-
-    @Override
-    public int compareTo(Path other)
+    public final int compareTo(final Path other)
     {
         return 0;
     }
