d6d48cc5029ee71ec870e2885d2114aa02ea4635
Francis Galiegue <fgaliegue@gmail.com>
2014-03-18 00:27:06 +0100

    Redo FTPPath from scratch, add FTPFileSystemProvider
    In fact, no method does anything at the moment...

--- src/main/java/com/github/fge/ftpfs/FTPPath.java
+++ src/main/java/com/github/fge/ftpfs/FTPPath.java
@@ -20,60 +20,174 @@ package com.github.fge.ftpfs;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.FileSystem;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.Iterator;
 
-public abstract class FTPPath
+public final class FTPPath
     implements Path
 {
-    private final FTPFileSystem fs;
-
-    protected FTPPath(final FTPFileSystem fs)
-    {
-        this.fs = fs;
-    }
-
     @Override
-    public final FileSystem getFileSystem()
-    {
-        return fs;
-    }
-
-    @Override
-    public final File toFile()
-    {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public final WatchKey register(final WatchService watcher,
-        final WatchEvent.Kind<?>[] events,
-        final WatchEvent.Modifier... modifiers)
-        throws IOException
-    {
-        throw new IOException("operation not supported");
-    }
-
-    @Override
-    public final WatchKey register(final WatchService watcher,
-        final WatchEvent.Kind<?>... events)
-        throws IOException
-    {
-        throw new IOException("operation not supported");
-    }
-
-    @Override
-    public final Iterator<Path> iterator()
+    public FileSystem getFileSystem()
     {
         return null;
     }
 
     @Override
-    public final int compareTo(final Path other)
+    public boolean isAbsolute()
+    {
+        return false;
+    }
+
+    @Override
+    public Path getRoot()
+    {
+        return null;
+    }
+
+    @Override
+    public Path getFileName()
+    {
+        return null;
+    }
+
+    @Override
+    public Path getParent()
+    {
+        return null;
+    }
+
+    @Override
+    public int getNameCount()
+    {
+        return 0;
+    }
+
+    @Override
+    public Path getName(int index)
+    {
+        return null;
+    }
+
+    @Override
+    public Path subpath(int beginIndex, int endIndex)
+    {
+        return null;
+    }
+
+    @Override
+    public boolean startsWith(Path other)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean startsWith(String other)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean endsWith(Path other)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean endsWith(String other)
+    {
+        return false;
+    }
+
+    @Override
+    public Path normalize()
+    {
+        return null;
+    }
+
+    @Override
+    public Path resolve(Path other)
+    {
+        return null;
+    }
+
+    @Override
+    public Path resolve(String other)
+    {
+        return null;
+    }
+
+    @Override
+    public Path resolveSibling(Path other)
+    {
+        return null;
+    }
+
+    @Override
+    public Path resolveSibling(String other)
+    {
+        return null;
+    }
+
+    @Override
+    public Path relativize(Path other)
+    {
+        return null;
+    }
+
+    @Override
+    public URI toUri()
+    {
+        return null;
+    }
+
+    @Override
+    public Path toAbsolutePath()
+    {
+        return null;
+    }
+
+    @Override
+    public Path toRealPath(LinkOption... options)
+        throws IOException
+    {
+        return null;
+    }
+
+    @Override
+    public File toFile()
+    {
+        return null;
+    }
+
+    @Override
+    public WatchKey register(WatchService watcher, WatchEvent.Kind<?>[] events,
+        WatchEvent.Modifier... modifiers)
+        throws IOException
+    {
+        return null;
+    }
+
+    @Override
+    public WatchKey register(WatchService watcher, WatchEvent.Kind<?>... events)
+        throws IOException
+    {
+        return null;
+    }
+
+    @Override
+    public Iterator<Path> iterator()
+    {
+        return null;
+    }
+
+    @Override
+    public int compareTo(Path other)
     {
         return 0;
     }
